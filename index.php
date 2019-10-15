58505c9ab4aa6bb7e0a0488030f881f0d714d1cc
Javier <javipons@mail.com>
2019-10-15 19:56:46 -0300

    se agregaron banners dinamicos

--- index.php
+++ index.php
@@ -13,12 +13,8 @@
     $query1 = new WP_Query($args);
     if($query1->have_posts() ) : while($query1 ->have_posts()) : $query1->the_post(); ?>
 
-    <?php $post_id = get_field('producto-destacado');?>
+    <?php $post_id = get_field('producto-destacado');
 
-    <?php endwhile; endif; 
-    
-   
-    
     $query2 = new WP_Query('post_type=iphone&p='. $post_id );
     while ( $query2->have_posts() ) : $query2->the_post(); ?>
 
@@ -26,6 +22,7 @@
         $cat = get_the_category()[0]->cat_name;
         $cat_id = get_cat_ID( $cat );
         $link = get_category_link( $cat_id );
+        $cuotas18 = get_field('18_cuotas');
         $nombre = get_field('iphone');
         if(get_field('precio_promocion')){
             $precio = get_field('precio_promocion');
@@ -33,24 +30,38 @@
             $precio = get_field('precio');
         }
         
+        
     ?>
-
-    <?php endwhile;?>
-<div class="banner-1">
-    <div class="container">
-        <div class="banner-1-texto">
-            <?php if (strpos($nombre, '11') !== false):?>
-            <h1><?php echo $nombre;?> a partir de U$<?php echo $precio; ?>***</h1>
-            <h3>Comprá en nuestro local abierto al público o recibilo por correo.</h3>
-            <a href="<?php echo $link;?>">Ver más ></a>
-            <?php else:?>
-            <h1><?php echo $nombre;?> a $<?php echo $precio; ?>.* o 12 cuotas de $<?php echo intval($precio*1.7/12);?>.*</h1>
-            <h3>Comprá en nuestro local abierto al público o recibilo por correo.</h3>
-            <a href="<?php echo $link;?>">Ver más ></a>
-            <?php endif;?>
+    
+    <div class="banner-1">
+        <div class="container">
+            <div class="banner-1-texto">
+                <?php if (strpos($nombre, '11') !== false):?>
+                <h1><?php echo $nombre;?> a partir de U$<?php echo $precio; ?>***</h1>
+                <h3>Comprá en nuestro local abierto al público o recibilo por correo.</h3>
+                <a href="<?php echo $link;?>">Ver más ></a>
+                <?php elseif ($cuotas18):?>
+                <h1><?php echo $nombre;?> en 18 cuotas de $<?php echo intval($precio*1.4/18);?>.*</h1>
+                <h3>Comprá en nuestro local abierto al público o recibilo por correo.</h3>
+                <a href="<?php echo $link;?>">Ver más ></a>
+                <?php else:?>
+                <h1><?php echo $nombre;?> a $<?php echo $precio; ?>.* o 12 cuotas de $<?php echo intval($precio*1.7/12);?>.*</h1>
+                <h3>Comprá en nuestro local abierto al público o recibilo por correo.</h3>
+                <a href="<?php echo $link;?>">Ver más ></a>
+                <?php endif;?>
+            </div>
         </div>
     </div>
-</div>
+
+    <?php endwhile;?>
+    
+
+    <?php endwhile; endif;?> 
+    
+   
+    
+    
+
 
 <div class="banner-2">
     <div class="container">
